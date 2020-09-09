05e5ea79df01c9e98f73027f9f81a792d1eb0d37
Gareth Emslie <gareth@soft-cor.es>
2020-09-09 12:47:50 +0200

    initial commit for wvd software

---
+++ wvd-agent/tools/chocolateyinstall.ps1
@@ -0,0 +1,51 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$pp = Get-PackageParameters
+
+if (!$pp['REGISTRATIONTOKEN']) { 
+  throw "Package needs parameter 'REGISTRATIONTOKEN' to install, that must be provided in params or in prompt."
+}
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'wvd-agent*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = '7A5C46DC754A183A7CA05E57FA8461493A4FA47E1535A80DE9DC893DF9374178'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/quiet /qn /norestart /passive REGISTRATIONTOKEN=`"$($pp['REGISTRATIONTOKEN']))`" /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
---
+++ wvd-boot-loader/tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'wvd-boot-loader*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = '8121C4808E07057FFE81C7C241FBA286F99C02AB5B47103F263BE40EEAA54C56'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/quiet /qn /norestart /passive /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
